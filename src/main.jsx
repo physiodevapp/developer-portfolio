7e8a21c662d12382a62f91e7045f83417c1efb4f
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-09-09 12:24:34 +0200

    style: add dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { PortfolioPage } from './pages/Portfolio/PortfolioPage.tsx';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme, GlobalStyles } from './styles/GlobalStyles.ts';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PortfolioPage/>
+    <ThemeProvider theme={darkTheme}>
+      <GlobalStyles/>
+      <PortfolioPage/>
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({theme}) => theme.body};
+    color: ${({theme}) => theme.text};
+    transition: background 0.2s ease-in, color 0.2s ease-in;
+  }
+
+`
+
+export const lightTheme = {
+  body: '#ffffff',
+  text: '#444452'
+}
+
+export const darkTheme = {
+  body: '#0f172a',
+  text: '#dde4ee'
+}
